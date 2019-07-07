34183e6a711e9c1b927ac55fbb5c1ca5b3d0060b
Luke Shiner <luke@lukeshiner.com>
2019-07-07 21:14:33 +0100

    feat: Add more operations to matricies

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,6 +1,10 @@
 package matrix
 
-import "github.com/lukeshiner/raytrace/comparison"
+import (
+	"errors"
+
+	"github.com/lukeshiner/raytrace/comparison"
+)
 
 // Matrix is a type for matricies.
 type Matrix struct {
@@ -33,6 +37,90 @@ func (m *Matrix) Column(index int) []float64 {
 		column = append(column, m.Cells[y][index])
 	}
 	return column
+}
+
+// Transpose returns the matrix created by transposing m.
+func (m Matrix) Transpose() Matrix {
+	var cells [][]float64
+	for i := 0; i < m.Height; i++ {
+		cells = append(cells, m.Column(i))
+	}
+	return New(cells...)
+}
+
+// Submatrix returns a Submatrix of m with row rowNumber and column columnNumber removed.
+func (m Matrix) Submatrix(rowNumber, columnNumber int) Matrix {
+	var cells [][]float64
+	var row []float64
+	for x := 0; x < m.Width; x++ {
+		if x != rowNumber {
+			for y := 0; y < m.Height; y++ {
+				if y != columnNumber {
+					row = append(row, m.Get(x, y))
+				}
+			}
+			cells = append(cells, make([]float64, len(row)))
+			copy(cells[len(cells)-1], row)
+			row = row[:0]
+		}
+	}
+	return New(cells...)
+}
+
+func (m Matrix) determinant() float64 {
+	if m.Width == 2 && m.Height == 2 {
+		return (m.Get(0, 0) * m.Get(1, 1)) - (m.Get(0, 1) * m.Get(1, 0))
+	}
+	const rowNumber = 0
+	var value float64
+	row := m.Row(rowNumber)
+	for i := 0; i < m.Width; i++ {
+		cofactor := m.cofactor(rowNumber, i)
+		value += cofactor * row[i]
+	}
+	return value
+}
+
+func (m Matrix) cofactor(rowNumber, columnNumber int) float64 {
+	minor := m.minor(rowNumber, columnNumber)
+	if (rowNumber+columnNumber)%2 != 0 {
+		minor = -minor
+	}
+	return minor
+}
+
+func (m Matrix) minor(rowNumber, columnNumber int) float64 {
+	submatrix := m.Submatrix(rowNumber, columnNumber)
+	return submatrix.determinant()
+}
+
+// Invertable returns true if the matrix is invertable, otherwise returns false.
+func (m Matrix) Invertable() bool {
+	return m.determinant() != 0
+}
+
+// Invert returns the inverse of the matrix
+func (m Matrix) Invert() (Matrix, error) {
+	if m.Invertable() == false {
+		err := errors.New("matrix is not invertable")
+		return Matrix{}, err
+	}
+	var cells [][]float64
+	var row []float64
+	var cofactor, value float64
+	determinant := m.determinant()
+	for x := 0; x < m.Width; x++ {
+		for y := 0; y < m.Height; y++ {
+			cofactor = m.cofactor(x, y)
+			value = cofactor / determinant
+			row = append(row, value)
+		}
+		cells = append(cells, make([]float64, len(row)))
+		copy(cells[len(cells)-1], row)
+		row = row[:0]
+	}
+	matrix := New(cells...)
+	return matrix.Transpose(), nil
 }
 
 // Equal compares two instances of Matrix for equality.
@@ -74,4 +162,40 @@ func Multiply(a, b Matrix) Matrix {
 	}
 	m := Matrix{Width: a.Width, Height: a.Height, Cells: values}
 	return m
+}
+
+func multiplyTupleCell(row, tuple []float64) float64 {
+	var value float64
+	for i := 0; i < len(row); i++ {
+		value += row[i] * tuple[i]
+	}
+	return value
+}
+
+// MultiplyTuple multiplies a Matrix by a tuple.
+func MultiplyTuple(m Matrix, tuple []float64) []float64 {
+	var value []float64
+	for x := 0; x < m.Height; x++ {
+		value = append(value, multiplyTupleCell(m.Row(x), tuple))
+	}
+	return value
+}
+
+// IdentityMatrix returns the identity matrix of size size.
+func IdentityMatrix(size int) Matrix {
+	var values [][]float64
+	var row []float64
+	for x := 0; x < size; x++ {
+		for y := 0; y < size; y++ {
+			if x == y {
+				row = append(row, 1)
+			} else {
+				row = append(row, 0)
+			}
+		}
+		values = append(values, make([]float64, len(row)))
+		copy(values[x], row)
+		row = row[:0]
+	}
+	return New(values...)
 }
--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -319,3 +319,424 @@ func TestTmp(t *testing.T) {
 	)
 	multiplyCell(a, b, 1, 0)
 }
+
+func TestMutliplyTuple(t *testing.T) {
+	var tests = []struct {
+		matrix   Matrix
+		tuple    []float64
+		expected []float64
+	}{
+		{
+			matrix: New(
+				[]float64{1, 2, 3, 4},
+				[]float64{2, 4, 4, 2},
+				[]float64{8, 6, 4, 1},
+				[]float64{0, 0, 0, 1},
+			),
+			tuple:    []float64{1, 2, 3, 1},
+			expected: []float64{18, 24, 33, 1},
+		},
+	}
+	for _, test := range tests {
+		value := MultiplyTuple(test.matrix, test.tuple)
+		for i := 0; i < len(test.expected); i++ {
+			if value[i] != test.expected[i] {
+				t.Errorf(
+					"Multiply Matrix %+v with Tuple %+v got %+v, expected %+v.",
+					test.matrix, test.tuple, value, test.expected,
+				)
+			}
+		}
+	}
+}
+
+func TestIdentityMatrix(t *testing.T) {
+	expected := New(
+		[]float64{1, 0, 0, 0},
+		[]float64{0, 1, 0, 0},
+		[]float64{0, 0, 1, 0},
+		[]float64{0, 0, 0, 1},
+	)
+	value := IdentityMatrix(4)
+	if Equal(value, expected) != true {
+		t.Errorf("Creating Identity Matrix of size 4 returned %+v", value)
+	}
+}
+
+func TestMultiplyByIdentity(t *testing.T) {
+	matrix := New(
+		[]float64{0, 1, 2, 4},
+		[]float64{1, 2, 4, 8},
+		[]float64{2, 4, 8, 16},
+		[]float64{4, 8, 16, 32},
+	)
+	identity := IdentityMatrix(4)
+	value := Multiply(matrix, identity)
+	if Equal(matrix, value) != true {
+		t.Errorf(
+			"Multiplying matrix %+v with the identity matrix returned %+v.",
+			matrix, value,
+		)
+	}
+}
+
+func TestMultiplyTupleByIdentity(t *testing.T) {
+	tuple := []float64{1, 2, 3, 4}
+	identity := IdentityMatrix(4)
+	value := MultiplyTuple(identity, tuple)
+	for i := 0; i < len(tuple); i++ {
+		if tuple[i] != value[i] {
+			t.Errorf(
+				"Multiplying tuple %+v with the identity matrix returned %+v.",
+				tuple, value,
+			)
+		}
+	}
+}
+
+func TestTransposeMethod(t *testing.T) {
+	var tests = []struct {
+		matrix   Matrix
+		expected Matrix
+	}{
+		{
+			matrix: New(
+				[]float64{0, 9, 3, 0},
+				[]float64{9, 8, 0, 8},
+				[]float64{1, 8, 5, 3},
+				[]float64{0, 0, 5, 8},
+			),
+			expected: New(
+				[]float64{0, 9, 1, 0},
+				[]float64{9, 8, 8, 0},
+				[]float64{3, 0, 5, 5},
+				[]float64{0, 8, 3, 8},
+			),
+		},
+		{
+			matrix:   IdentityMatrix(4),
+			expected: IdentityMatrix(4),
+		},
+	}
+	for _, test := range tests {
+		value := test.matrix.Transpose()
+		if Equal(value, test.expected) != true {
+			t.Errorf(
+				"Transposed Matrix %+v got %+v, expected %+v.",
+				test.matrix, value, test.expected,
+			)
+		}
+	}
+}
+
+func TestDeterminant2x2(t *testing.T) {
+	var tests = []struct {
+		matrix   Matrix
+		expected float64
+	}{
+		{
+			matrix: New(
+				[]float64{1, 5},
+				[]float64{-3, 2},
+			),
+			expected: 17,
+		},
+	}
+	for _, test := range tests {
+		value := test.matrix.determinant()
+		if value != test.expected {
+			t.Errorf(
+				"Determinant of %+v: got %+v, expected %+v",
+				test.matrix, value, test.expected)
+		}
+	}
+}
+
+func TestSubmatrix(t *testing.T) {
+	var tests = []struct {
+		matrix, expected Matrix
+		row, column      int
+	}{
+		{
+			matrix: New(
+				[]float64{1, 5, 0},
+				[]float64{-3, 2, 7},
+				[]float64{0, 6, -3},
+			),
+			row:    0,
+			column: 2,
+			expected: New(
+				[]float64{-3, 2},
+				[]float64{0, 6},
+			),
+		},
+	}
+	for _, test := range tests {
+		value := test.matrix.Submatrix(test.row, test.column)
+		if Equal(value, test.expected) != true {
+			t.Errorf(
+				"Submatrix (%d, %d) of matrix %+v was %+v, expected %+v",
+				test.row, test.column, test.matrix, value, test.expected,
+			)
+		}
+	}
+}
+
+func TestMinor3x3(t *testing.T) {
+	var tests = []struct {
+		matrix      Matrix
+		row, column int
+		expected    float64
+	}{
+		{
+			matrix: New(
+				[]float64{3, 5, 0},
+				[]float64{2, -1, 7},
+				[]float64{6, -1, 5},
+			),
+			row:      1,
+			column:   0,
+			expected: 25,
+		},
+	}
+	for _, test := range tests {
+		value := test.matrix.minor(test.row, test.column)
+		if value != test.expected {
+			t.Errorf(
+				"Minor of matrix %+v (%d, %d): got %+v, expected %+v.",
+				test.matrix, test.row, test.column, value, test.expected,
+			)
+		}
+	}
+}
+
+func TestCofactor3x3(t *testing.T) {
+	var tests = []struct {
+		matrix      Matrix
+		row, column int
+		expected    float64
+	}{
+		{
+			matrix: New(
+				[]float64{3, 5, 0},
+				[]float64{2, -1, -7},
+				[]float64{6, -1, 5},
+			),
+			row:      1,
+			column:   0,
+			expected: -25,
+		},
+	}
+	for _, test := range tests {
+		value := test.matrix.cofactor(test.row, test.column)
+		if value != test.expected {
+			t.Errorf(
+				"Cofactor of matrix %+v (%d, %d): got %+v, expected %+v.",
+				test.matrix, test.row, test.column, value, test.expected,
+			)
+		}
+	}
+}
+
+func TestDeterminant(t *testing.T) {
+	type determinat3x3test struct {
+		row, column int
+		expected    float64
+	}
+	var tests = []struct {
+		matrix              Matrix
+		row, column         int
+		cofactorTests       []determinat3x3test
+		expectedDeterminant float64
+	}{
+		{
+			matrix: New(
+				[]float64{1, 2, 6},
+				[]float64{-5, 8, -4},
+				[]float64{2, 6, 4},
+			),
+			cofactorTests: []determinat3x3test{
+				{row: 0, column: 0, expected: 56},
+				{row: 0, column: 1, expected: 12},
+				{row: 0, column: 2, expected: -46},
+			},
+			expectedDeterminant: -196,
+		},
+		{
+			matrix: New(
+				[]float64{-2, -8, 3, 5},
+				[]float64{-3, 1, 7, 3},
+				[]float64{1, 2, -9, 6},
+				[]float64{-6, 7, 7, -9},
+			),
+			cofactorTests: []determinat3x3test{
+				{row: 0, column: 0, expected: 690},
+				{row: 0, column: 1, expected: 447},
+				{row: 0, column: 2, expected: 210},
+				{row: 0, column: 3, expected: 51},
+			},
+			expectedDeterminant: -4071,
+		},
+	}
+	for _, test := range tests {
+		for _, cofactorTest := range test.cofactorTests {
+			cValue := test.matrix.cofactor(cofactorTest.row, cofactorTest.column)
+			if cValue != cofactorTest.expected {
+				t.Errorf(
+					"Matrix %+v cofactor (%d, %d) was %+v, expected %+v.",
+					test.matrix, cofactorTest.row, cofactorTest.column,
+					cValue, cofactorTest.expected,
+				)
+			}
+		}
+		value := test.matrix.determinant()
+		if value != test.expectedDeterminant {
+			t.Errorf(
+				"Matrix %+v determinant was %+v, expected %+v.",
+				test.matrix, value, test.expectedDeterminant,
+			)
+		}
+	}
+}
+
+func TestInvertable(t *testing.T) {
+	var tests = []struct {
+		matrix      Matrix
+		determinant float64
+		invertable  bool
+	}{
+		{
+			matrix: New(
+				[]float64{6, 4, 4, 4},
+				[]float64{5, 5, 7, 6},
+				[]float64{4, -9, 3, -7},
+				[]float64{9, 1, 7, -6},
+			),
+			determinant: -2120,
+			invertable:  true,
+		},
+		{
+			matrix: New(
+				[]float64{-4, 2, -2, -3},
+				[]float64{9, 6, 2, 6},
+				[]float64{0, -5, 1, -5},
+				[]float64{0, 0, 0, 0},
+			),
+			determinant: 0,
+			invertable:  false,
+		},
+	}
+	for _, test := range tests {
+		determinantValue := test.matrix.determinant()
+		if determinantValue != test.determinant {
+			t.Errorf(
+				"The determinant of matrix %+v was %+v, expected %+v",
+				test.matrix, determinantValue, test.determinant,
+			)
+		}
+		invertableValue := test.matrix.Invertable()
+		if invertableValue != test.invertable {
+			t.Errorf(
+				"Matrix.Invertable for matrix %+v was %+v, expected %+v",
+				test.matrix, invertableValue, test.invertable,
+			)
+		}
+	}
+}
+
+func TestInvert(t *testing.T) {
+	var tests = []struct {
+		matrix  Matrix
+		inverse Matrix
+	}{
+		{
+			matrix: New(
+				[]float64{-5, 2, 6, -8},
+				[]float64{1, -5, 1, 8},
+				[]float64{7, 7, -6, -7},
+				[]float64{1, -3, 7, 4},
+			),
+			inverse: New(
+				[]float64{0.21805, 0.45113, 0.24060, -0.04511},
+				[]float64{-0.80827, -1.45677, -0.44361, 0.52068},
+				[]float64{-0.07895, -0.22368, -0.05263, 0.19737},
+				[]float64{-0.52256, -0.81391, -0.30075, 0.30639},
+			),
+		},
+		{
+			matrix: New(
+				[]float64{8, -5, 9, 2},
+				[]float64{7, 5, 6, 1},
+				[]float64{-6, 0, 9, 6},
+				[]float64{-3, 0, -9, -4},
+			),
+			inverse: New(
+				[]float64{-0.15385, -0.15385, -0.28205, -0.53846},
+				[]float64{-0.07692, 0.12308, 0.02564, 0.03077},
+				[]float64{0.35897, 0.35897, 0.43590, 0.92308},
+				[]float64{-0.69231, -0.69231, -0.76923, -1.92308},
+			),
+		},
+		{
+			matrix: New(
+				[]float64{9, 3, 0, 9},
+				[]float64{-5, -2, -6, -3},
+				[]float64{-4, 9, 6, 4},
+				[]float64{-7, 6, 6, 2},
+			),
+			inverse: New(
+				[]float64{-0.04074, -0.07778, 0.14444, -0.22222},
+				[]float64{-0.07778, 0.03333, 0.36667, -0.33333},
+				[]float64{-0.02901, -0.14630, -0.10926, 0.12963},
+				[]float64{0.17778, 0.06667, -0.26667, 0.33333},
+			),
+		},
+		{
+			matrix: New(
+				[]float64{-4, 2, -2, -3},
+				[]float64{9, 6, 2, 6},
+				[]float64{0, -5, 1, -5},
+				[]float64{0, 0, 0, 0},
+			),
+			inverse: Matrix{},
+		},
+	}
+	for _, test := range tests {
+		value, _ := test.matrix.Invert()
+		if Equal(value, test.inverse) != true {
+			t.Errorf(
+				"Inverse of matrix %+v was %+v, expected %+v.",
+				test.matrix, value, test.inverse,
+			)
+		}
+	}
+}
+
+func TestMultiplyByInverse(t *testing.T) {
+	var tests = []struct {
+		a, b Matrix
+	}{
+		{
+			a: New(
+				[]float64{3, -9, 7, 3},
+				[]float64{3, -8, 2, -9},
+				[]float64{-4, 4, 4, 1},
+				[]float64{-6, 5, -1, 1},
+			),
+			b: New(
+				[]float64{8, 2, 2, 2},
+				[]float64{3, -1, 7, 0},
+				[]float64{7, 0, 5, 4},
+				[]float64{6, -2, 0, 5},
+			),
+		},
+	}
+	for _, test := range tests {
+		c := Multiply(test.a, test.b)
+		inverse, _ := test.b.Invert()
+		value := Multiply(c, inverse)
+		if Equal(test.a, value) != true {
+			t.Errorf("Failed get back original matrix by multiplying with the inverse.")
+		}
+	}
+}

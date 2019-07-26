331f2dacb27549f1fb3d2c922446f9590057ca5f
Luke Shiner <luke@lukeshiner.com>
2019-07-26 19:16:50 +0100

    feat: Add ray package

---
+++ ray/ray.go
@@ -0,0 +1,138 @@
+package ray
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"sort"
+
+	"github.com/lukeshiner/raytrace/matrix"
+	"github.com/lukeshiner/raytrace/vector"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func generateID() string {
+	b := make([]rune, 100)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+// Ray is the struct for raytracer rays.
+type Ray struct {
+	Origin, Direction vector.Vector
+}
+
+// Position returns the position of the ray at time t.
+func (r *Ray) Position(t float64) vector.Vector {
+	return vector.Add(r.Origin, r.Direction.ScalarMultiply(t))
+}
+
+// Transform transforms a ray by a transform matrix.
+func (r *Ray) Transform(m matrix.Matrix) Ray {
+	origin := vector.MultiplyMatrixByVector(m, r.Origin)
+	direction := vector.MultiplyMatrixByVector(m, r.Direction)
+	return New(origin, direction)
+}
+
+// Sphere is the struct for spheres
+type Sphere struct {
+	ID        string
+	Transform matrix.Matrix
+}
+
+// SetTransform sets the transform matrix for a sphere.
+func (s *Sphere) SetTransform(m matrix.Matrix) {
+	s.Transform = m
+}
+
+// Intersection holds an intersection.
+type Intersection struct {
+	T      float64
+	Object Sphere
+}
+
+// Intersections holds a slice of Intersection.
+type Intersections struct {
+	Intersections []Intersection
+}
+
+// Count returns the number of intersections.
+func (i *Intersections) Count() int {
+	return len(i.Intersections)
+}
+
+// Get returns an intersection by index
+func (i *Intersections) Get(index int) Intersection {
+	return i.Intersections[index]
+}
+
+// TSlice returns a slice of t values for the intersections.
+func (i *Intersections) TSlice() []float64 {
+	var ts []float64
+	for x := 0; x < len(i.Intersections); x++ {
+		ts = append(ts, i.Get(x).T)
+	}
+	return ts
+}
+
+func (i Intersections) sort() []Intersection {
+	sort.Slice(i.Intersections, func(j, k int) bool {
+		return i.Intersections[j].T < i.Intersections[k].T
+	})
+	return i.Intersections
+}
+
+// Hit returns the first hit in the intersections or an error if there are none.
+func (i *Intersections) Hit() (Intersection, error) {
+	i.sort()
+	for _, intersection := range i.Intersections {
+		if intersection.T >= 0 {
+			return intersection, nil
+		}
+	}
+	return NewIntersection(0, NewSphere()), fmt.Errorf("No intersections hit")
+}
+
+// NewIntersection returns an Intersection instance.
+func NewIntersection(t float64, object Sphere) Intersection {
+	return Intersection{T: t, Object: object}
+}
+
+// NewIntersections returns a new Intersections list.
+func NewIntersections(i ...Intersection) Intersections {
+	intersections := Intersections{Intersections: i}
+	intersections.sort()
+	return intersections
+}
+
+// New creates a new Ray struct.
+func New(origin, direction vector.Vector) Ray {
+	return Ray{Origin: origin, Direction: direction}
+}
+
+// NewSphere returns a unit sphere at the origin
+func NewSphere() Sphere {
+	return Sphere{ID: generateID(), Transform: matrix.IdentityMatrix(4)}
+}
+
+// Intersect returns a list of intersectioins between ray and sphere.
+func Intersect(sphere Sphere, ray Ray) Intersections {
+	transform, _ := sphere.Transform.Invert()
+	tRay := ray.Transform(transform)
+	sphereToRay := vector.Subtract(tRay.Origin, vector.NewPoint(0, 0, 0))
+	a := vector.DotProduct(tRay.Direction, tRay.Direction)
+	b := 2 * vector.DotProduct(tRay.Direction, sphereToRay)
+	c := vector.DotProduct(sphereToRay, sphereToRay) - 1
+	discriminant := (b * b) - 4*a*c
+	if discriminant < 0 {
+		return Intersections{}
+	}
+	t1 := (-b - math.Sqrt(discriminant)) / (2 * a)
+	i1 := NewIntersection(t1, sphere)
+	t2 := (-b + math.Sqrt(discriminant)) / (2 * a)
+	i2 := NewIntersection(t2, sphere)
+	return NewIntersections(i1, i2)
+}
---
+++ ray/ray_test.go
@@ -0,0 +1,281 @@
+package ray
+
+import (
+	"testing"
+
+	"github.com/lukeshiner/raytrace/comparison"
+	"github.com/lukeshiner/raytrace/matrix"
+	"github.com/lukeshiner/raytrace/vector"
+)
+
+func TestRay(t *testing.T) {
+	var tests = []struct {
+		origin, direction vector.Vector
+	}{
+		{
+			origin:    vector.NewPoint(1, 2, 3),
+			direction: vector.NewVector(4, 5, 6),
+		},
+	}
+	for _, test := range tests {
+		ray := New(test.origin, test.direction)
+		if vector.Equal(test.origin, ray.Origin) != true {
+			t.Errorf("Ray origin was %+v, expected %+v.", ray.Origin, test.origin)
+		}
+		if vector.Equal(test.direction, ray.Direction) != true {
+			t.Errorf("Ray direction was %+v, expected %+v.", ray.Direction, test.direction)
+		}
+	}
+}
+
+func TestPosition(t *testing.T) {
+	var tests = []struct {
+		ray      Ray
+		t        float64
+		expected vector.Vector
+	}{
+		{
+			ray:      New(vector.NewPoint(2, 3, 4), vector.NewVector(1, 0, 0)),
+			t:        0,
+			expected: vector.NewPoint(2, 3, 4),
+		},
+		{
+			ray:      New(vector.NewPoint(2, 3, 4), vector.NewVector(1, 0, 0)),
+			t:        1,
+			expected: vector.NewPoint(3, 3, 4),
+		},
+		{
+			ray:      New(vector.NewPoint(2, 3, 4), vector.NewVector(1, 0, 0)),
+			t:        -1,
+			expected: vector.NewPoint(1, 3, 4),
+		},
+		{
+			ray:      New(vector.NewPoint(2, 3, 4), vector.NewVector(1, 0, 0)),
+			t:        2.5,
+			expected: vector.NewPoint(4.5, 3, 4),
+		},
+	}
+	for _, test := range tests {
+		result := test.ray.Position(test.t)
+		if vector.Equal(result, test.expected) != true {
+			t.Errorf(
+				"Ray %+v at position %+v was %+v, expected %+v.",
+				test.ray, test.t, result, test.expected,
+			)
+		}
+	}
+}
+
+func TestIntersect(t *testing.T) {
+	var tests = []struct {
+		ray      Ray
+		sphere   Sphere
+		expected []float64
+	}{
+		{
+			ray:      New(vector.NewPoint(0, 0, -5), vector.NewVector(0, 0, 1)),
+			sphere:   NewSphere(),
+			expected: []float64{4.0, 6.0},
+		},
+		{
+			ray:      New(vector.NewPoint(0, 1, -5), vector.NewVector(0, 0, 1)),
+			sphere:   NewSphere(),
+			expected: []float64{5.0, 5.0},
+		},
+		{
+			ray:      New(vector.NewPoint(0, 2, -5), vector.NewVector(0, 0, 1)),
+			sphere:   NewSphere(),
+			expected: []float64{},
+		},
+		{
+			ray:      New(vector.NewPoint(0, 0, 0), vector.NewVector(0, 0, 1)),
+			sphere:   NewSphere(),
+			expected: []float64{-1.0, 1.0},
+		},
+		{
+			ray:      New(vector.NewPoint(0, 0, 5), vector.NewVector(0, 0, 1)),
+			sphere:   NewSphere(),
+			expected: []float64{-6.0, -4.0},
+		},
+	}
+	for _, test := range tests {
+		intersections := Intersect(test.sphere, test.ray)
+		xs := intersections.TSlice()
+		if comparison.EqualSlice(xs, test.expected) != true {
+			t.Errorf(
+				"Intersection of Ray %+v and Shpere %+v was %+v, expected %+v.",
+				test.ray, test.sphere, xs, test.expected,
+			)
+		}
+	}
+}
+
+func TestIntersection(t *testing.T) {
+	var tests = []struct {
+		t float64
+		o Sphere
+	}{
+		{
+			t: 3.5,
+			o: NewSphere(),
+		},
+	}
+	for _, test := range tests {
+		intersection := Intersection{test.t, test.o}
+		if intersection.T != test.t || intersection.Object.ID != test.o.ID {
+			t.Error("Error creating Intersection")
+		}
+	}
+}
+
+func TestIntersections(t *testing.T) {
+	s := NewSphere()
+	i1 := Intersection{1, s}
+	i2 := Intersection{2, s}
+	xs := Intersections{[]Intersection{i1, i2}}
+	if xs.Count() != 2 || xs.Get(0).T != 1 || xs.Get(1).T != 2 {
+		t.Error("Intersect did not set object.")
+	}
+}
+
+func TestIntersectionsSetsObject(t *testing.T) {
+	ray := New(vector.NewPoint(0, 0, -5), vector.NewVector(0, 0, 1))
+	sphere := NewSphere()
+	xs := Intersect(sphere, ray)
+	if xs.Get(0).Object.ID != sphere.ID || xs.Get(1).Object.ID != sphere.ID {
+		t.Error("Intersect did not set object.")
+	}
+}
+
+func TestHit(t *testing.T) {
+	var tests = []struct {
+		intersections Intersections
+		expectNil     bool
+		expected      float64
+	}{
+		{
+			intersections: NewIntersections(
+				NewIntersection(1, NewSphere()),
+				NewIntersection(2, NewSphere()),
+			),
+			expectNil: false,
+			expected:  1,
+		},
+		{
+			intersections: NewIntersections(
+				NewIntersection(-1, NewSphere()),
+				NewIntersection(1, NewSphere()),
+			),
+			expectNil: false,
+			expected:  1,
+		},
+		{
+			intersections: NewIntersections(
+				NewIntersection(-2, NewSphere()),
+				NewIntersection(-1, NewSphere()),
+			),
+			expectNil: true,
+		},
+		{
+			intersections: NewIntersections(
+				NewIntersection(5, NewSphere()),
+				NewIntersection(7, NewSphere()),
+				NewIntersection(-3, NewSphere()),
+				NewIntersection(2, NewSphere()),
+			),
+			expectNil: false,
+			expected:  2,
+		},
+	}
+	for _, test := range tests {
+		result, err := test.intersections.Hit()
+		if err != nil && test.expectNil != true {
+			t.Errorf(
+				"Intersections %+v had a hit when none was expected",
+				test.intersections,
+			)
+		} else {
+			if result.T != test.expected {
+				t.Errorf(
+					"Intersections %+v hit was %v, expected %v.",
+					test.intersections, result.T, test.expected,
+				)
+			}
+		}
+	}
+}
+
+func TestTransformRay(t *testing.T) {
+	var tests = []struct {
+		ray      Ray
+		m        matrix.Matrix
+		expected Ray
+	}{
+		{
+			ray:      New(vector.NewPoint(1, 2, 3), vector.NewVector(0, 1, 0)),
+			m:        matrix.TranslationMatrix(3, 4, 5),
+			expected: New(vector.NewPoint(4, 6, 8), vector.NewVector(0, 1, 0)),
+		},
+		{
+			ray:      New(vector.NewPoint(1, 2, 3), vector.NewVector(0, 1, 0)),
+			m:        matrix.ScalingMatrix(2, 3, 4),
+			expected: New(vector.NewPoint(2, 6, 12), vector.NewVector(0, 3, 0)),
+		},
+	}
+	for _, test := range tests {
+		result := test.ray.Transform(test.m)
+		if vector.Equal(result.Origin, test.expected.Origin) != true ||
+			vector.Equal(result.Direction, test.expected.Direction) != true {
+			t.Errorf(
+				"Transforming ray %+v with matrix %+v results in %+v, expected %+v.",
+				test.ray, test.m, result, test.expected,
+			)
+		}
+	}
+}
+
+func TestDefaultSphereTransform(t *testing.T) {
+	s := NewSphere()
+	if matrix.Equal(s.Transform, matrix.IdentityMatrix(4)) != true {
+		t.Error("Sphere default transform was not the identity matrix.")
+	}
+}
+
+func TestSetTransform(t *testing.T) {
+	s := NewSphere()
+	transform := matrix.TranslationMatrix(2, 3, 4)
+	s.SetTransform(transform)
+	if matrix.Equal(s.Transform, transform) != true {
+		t.Error("Did not set transform on sphere.")
+	}
+}
+
+func TestIntersectionWithTransformedSphere(t *testing.T) {
+	var tests = []struct {
+		ray       Ray
+		transform matrix.Matrix
+		expected  []float64
+	}{
+		{
+			ray:       New(vector.NewPoint(0, 0, -5), vector.NewVector(0, 0, 1)),
+			transform: matrix.ScalingMatrix(2, 2, 2),
+			expected:  []float64{3, 7},
+		},
+		{
+			ray:       New(vector.NewPoint(0, 0, -5), vector.NewVector(0, 0, 1)),
+			transform: matrix.TranslationMatrix(5, 0, 0),
+			expected:  []float64{},
+		},
+	}
+	for _, test := range tests {
+		s := NewSphere()
+		s.SetTransform(test.transform)
+		xs := Intersect(s, test.ray)
+		if comparison.EqualSlice(xs.TSlice(), test.expected) != true {
+			t.Errorf(
+				"Intersection of ray %+v with sphere transformed by %+v gave %+v, expected %+v.",
+				test.ray, test.transform, xs.TSlice(), test.expected,
+			)
+		}
+	}
+}
